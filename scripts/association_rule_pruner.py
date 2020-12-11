3227b0abe6b7e3d51ae8ce5bc0c5426d82d83825
Eli Verbrugge <eli.verbrugge@gmail.com>
2020-12-11 16:10:34 -0600

    Added pruning scripts

---
+++ scripts/association_rule_pruner.py
@@ -0,0 +1,208 @@
+import operator
+import numpy as np
+import pandas as pd
+from statsmodels.stats.proportion import proportion_confint
+
+
+OPERATOR_MAP = {
+    0: operator.lt,
+    1: operator.le,
+    2: operator.gt,
+    3: operator.ge,
+    4: operator.eq,
+    5: operator.ne    
+}
+
+
+def _get_rule_index_pair(df, encoded_rule):
+    """
+    Returns a tuple of pd.Index, (idx, not_idx) such that 
+    For all in idx the rule is true in df 
+    and for all in not_idx the rule is false in df
+
+    df              : Dataframe to be used
+    encoded_rule    : Encoded rule, [column_idx, op_code, value]
+    """
+    col_idx, op_code, v = encoded_rule
+    col = df.columns[col_idx]
+    op = OPERATOR_MAP[op_code]
+
+    rule = pd.Index(np.where(op(df[col], v))[0])
+    not_rule = df.index.difference(rule)
+    return (rule, not_rule)
+
+
+def _get_rule_index_pairs(df, encoded_rules):
+    """
+    Returns list of tuples of index pairs, as specified by 
+    """
+    return [_get_rule_index_pair(df, encoded_rule) for encoded_rule in encoded_rules]
+
+
+def _intersect_idxs(idxs):
+    """
+    Returns the intersection of an in indexable iterable of indexes
+    """
+    if len(idxs) == 1:
+        return idxs[0]
+    elif len(idxs) == 0:
+        return pd.Index([])
+    else:
+        return idxs[0].intersection(_intersect_idxs(idxs[1:]))
+
+
+def error_rate(errors, trials, significance):
+    """
+    Returns the expected error for a given amount of errors and trials for a specified signifigance
+    errors [0, trials]
+    trials [1, inf)
+    significance (0, 1)
+    """
+    ci_low, _ = proportion_confint(trials-errors, trials, significance, method='beta')
+    return (1 - ci_low)
+
+
+def _calc_yn_en(ante_pairs, cnsq_pairs, n=None):
+    """
+    Calculates y_n and e_n given a pairs index & not_indexes 
+    for antecedents and consequents
+
+    When n is given, the n'th term in the antecedent is negated
+    If n is not given, no term in the antecedent is negated.
+
+    Returns (y, e)
+    """
+    # Generate indexes for c and !c
+    cnsq = _intersect_idxs([c[0] for c in cnsq_pairs])
+    not_cnsq = _intersect_idxs([c[1] for c in cnsq_pairs])
+
+    # Determine indexes where A1, A2, .., !An, .., Am-1, Am is true
+    ante = [a[0] for a in ante_pairs]
+    if n is not None:
+        ante[n] = ante_pairs[n][1]
+    idxs = _intersect_idxs(ante)
+
+    # Find where c (y), and !c (e)
+    y = cnsq.intersection(idxs).size
+    e = not_cnsq.intersection(idxs).size
+    return (y, e)
+
+
+def _calc_rule_error(ys, es, confidence):
+    """
+    Calcuates the expected error of a rule given a list 
+    of y's and e's and a confidence level
+    """
+    errors = sum(es)
+    trials = sum(ys) + errors
+    return error_rate(errors, trials, confidence)
+
+
+
+def _drop_condition(ante_pairs, cnsq_pairs, confidence):
+    """
+    Returns the index of the condition in the ante
+    """
+    if len(ante_pairs) == 0:
+        # No rules to drop, return None
+        return None
+
+    # Calculate predicted error without modification of rule
+    y, e = _calc_yn_en(ante_pairs, cnsq_pairs)
+    if (y + e) == 0:
+        # If no instances in dataset, don't modify rule
+        # Return None as no condition should be dropped
+        return None
+
+    base_error = _calc_rule_error([y], [e], confidence)
+    rule_errors = []
+
+    # Calculate error for each rule
+    for n in range(len(ante_pairs)):
+        yn, en = _calc_yn_en(ante_pairs, cnsq_pairs, n=n)
+        rule_errors.append(_calc_rule_error([y, yn], [e, en], confidence))
+
+    if min(rule_errors) < base_error:
+        return rule_errors.index(min(rule_errors))
+    else:
+        return None
+
+
+
+def _simplify_rule(df, rule, confidence):
+    """
+    Returns a simplified rule with <= predicted error rate using 
+    confidence value specified
+    """
+    ante, cnsq = rule
+    ante_pairs = _get_rule_index_pairs(df, ante)    
+    cnsq_pairs = _get_rule_index_pairs(df, cnsq)    
+
+    # Walrus operator. Requires Python 3.8+
+    while (cond_to_drop := _drop_condition(ante_pairs, cnsq_pairs, confidence)):
+        # If condition to drop is not None, pop condition
+        ante.pop(cond_to_drop)
+        ante_pairs.pop(cond_to_drop)
+    return [ante, cnsq]
+
+
+
+def simplify_rules(df, rules, confidence):
+    """
+    Simplifies a set to rules based on a given confidence interval
+
+    df      : Dataframe that the 
+    """
+    new_rules = []
+
+    for idx, rule in enumerate(rules):
+        simplified_rule = _simplify_rule(df, rule, confidence)
+        if simplified_rule not in new_rules:
+            # Don't add duplicate rules
+            new_rules.append(simplified_rule)
+
+    # Sort rules and return
+    return sorted(new_rules, key=lambda x: str(x[0]))
+
+
+
+if __name__ == '__main__':
+    from copy import deepcopy
+    CSV_PATH = r'.\credit_output_binned.csv'
+
+    df = pd.read_csv(CSV_PATH)
+
+    rules = [[[[10, 4, 'female div/dep/mar 278']], [[19, 4, 1]]], [[[7, 4, '(-0.001, 7.154]'], [15, 4, 'none'], [18, 4, 'skilled']], [[19, 4, 1]]], [[[2, 4, 5], [15, 4, 'none'], [18, 4, 'skilled']], [[19, 4, 1]]],
+        [[[7, 4, '(-0.001, 7.154]'], [11, 4, 'none'], [18, 4, 'skilled']], [[19, 4, 1]]],
+        [[[2, 4, 5], [15, 4, 'none'], [24, 4, 'GOOD']], [[19, 4, 1]]],
+        [[[7, 4, '(-0.001, 7.154]'], [11, 4, 'none'], [16, 4, 'own']], [[2, 4, 5]]],
+        [[[11, 4, 'none'], [15, 4, 'none'], [18, 4, 'skilled']], [[19, 4, 1]]],
+        [[[7, 4, '(-0.001, 7.154]'], [11, 4, 'none'], [17, 4, 1]], [[19, 4, 1]]],
+        [[[2, 4, 5], [15, 4, 'none'], [21, 4, 'YES']], [[19, 4, 1]]],
+        [[[15, 4, 'none'], [16, 4, 'own'], [18, 4, 'skilled']], [[19, 4, 1]]],
+    ]
+
+    print(f'Original ruleset has {len(rules)} rules.')
+    print()
+    # Generate simplified rules
+    print('Creating rules for 95% confidence')
+    rules_95_conf = simplify_rules(df, deepcopy(rules), .95)
+    print(f'95% confidence ruleset has {len(rules_95_conf)} rules.')
+    print()
+    print('Creating rules for 25% confidence')
+    rules_25_conf = simplify_rules(df, deepcopy(rules), .25)
+    print(f'25% confidence ruleset has {len(rules_25_conf)} rules.')
+    print()
+    print("Writing rulesets to file...")
+
+
+    # Write simplified rules to file
+    with open(r'rules_95_conf.txt', 'w') as outfile:
+        outfile.writelines([str(rule) + '\n' for rule in rules_95_conf])
+
+    with open(r'rules_25_conf.txt', 'w') as outfile:
+        outfile.writelines([str(rule) + '\n' for rule in rules_25_conf])
+
+    print("Done!")
+
+    
---
+++ scripts/decision_tree_pruner.py
@@ -0,0 +1,229 @@
+import pandas as pd
+import numpy as np
+import sys
+import re
+import argparse
+import ast
+from statsmodels.stats.proportion import proportion_confint
+
+operator_to_int = {
+ "<": 0,
+ "<=": 1,
+ ">": 2,
+ ">=": 3,
+ "=": 4,
+ "==": 4,
+ "!=": 5,
+}
+int_to_operator_opposite = {
+ 0: ">=",
+ 1: ">",
+ 2: "<=",
+ 3: "<",
+ 4: "!=",
+ 5: "==",
+}
+int_to_operator = {
+ 0: "<" ,
+ 1: "<=",
+ 2: ">",
+ 3: ">=",
+ 4: "==",
+ 5: "!=",
+} 
+
+def format_rule_entity(column_names, line):
+ entity: list = []
+ # Split on operators, but still keep the seperator
+ if "<=" in line or ">=" in line or "==" in line:
+     elements = re.split('(<=|>=|==|:)',line)
+ else:
+    elements = re.split('(<|<=|>|>=|=|==|!=|:)',line)
+
+ print(elements)
+ # Append the attribute index
+ entity.append(column_names.index(elements[0].strip()))
+ # Append the enumeration of the operator
+ entity.append(operator_to_int[elements[1]])
+ # Append the value itself
+ entity.append(elements[2].strip('\n '))
+ #print(entity)
+ #print(line)
+ return entity
+ 
+def generate_rules(df, rulesFile):
+ ruleLines = rulesFile.readlines()
+ column_names: list = []
+ for name in df.columns:
+    column_names.append(name)
+
+ # Create a list of parent antecendents for our row that we modify on the fly
+ antecedents: list = []
+ rules: list = []
+ level: int = 0
+ for line in ruleLines:
+    num_vertical_bar = line.count('|')
+    line = line.replace('|', '')
+    if num_vertical_bar < level and len(antecedents) > 0:
+        # We have moved back one level, remove an antecedent from our list
+        diff = level - num_vertical_bar
+        for i in range(diff):
+            elem = antecedents.pop()
+    # If we find a : we have found the end of a rule
+    if ":" in line:
+        #print(line)
+        full_antecedents = antecedents.copy()
+
+        elements = re.split(':',line) 
+
+        full_antecedents.append(format_rule_entity(column_names, elements[0]))
+        consequent = [len(column_names)-1, operator_to_int["="], elements[1].strip('\n ')]
+
+        new_rule = [full_antecedents, consequent]
+        rules.append(new_rule)
+    else:
+        # Add an entity to the list of antecdents as we haven't reached a leaf
+        antecedents.append(format_rule_entity(column_names, line))
+
+    level = num_vertical_bar
+
+ f = open("rules_gen.txt", "w")
+ for rule in rules:
+    f.write(str(rule)+"\n")
+
+def compare(df, column_name, operator, value):
+ '''
+ Returns a dataframe that has had the following condition: column_name
+ operator (can be ==, !=, >, etc) value
+ '''
+ value_pos = re.sub("^-", "", value)
+ if str.isdigit(value_pos):
+     value = float(value)
+ if operator == ">":
+    return df.loc[df[column_name] > value]
+ elif operator == ">=":
+    return df.loc[df[column_name] >= value]
+ elif operator == "<":
+    print(value)
+    return df.loc[df[column_name] < value]
+ elif operator == "<=":
+    return df.loc[df[column_name] <= value]
+ elif operator == "==":
+    return df.loc[df[column_name] == value]
+ else:
+    return df.loc[df[column_name] != value]
+
+def find_occurences(df, antecedents, consequent, pos):
+ '''
+ Find the number of rows that match the antecedent and consequent, with
+ pos being a list of conditions
+ that should be inverted
+ '''
+ column_names: list = []
+ for name in df.columns:
+    column_names.append(str(name))
+
+ index = 0
+ df_new = df.copy()
+ for antecedent in antecedents:
+    value = antecedent[2]
+    if index in pos:
+        df_new = compare(df_new, column_names[antecedent[0]], int_to_operator_opposite[antecedent[1]], value)
+    else:
+        df_new = compare(df_new, column_names[antecedent[0]], int_to_operator[antecedent[1]], value)
+        index += 1
+ 
+ value = consequent[2]
+ if -1 in pos:
+    df_new = compare(df_new, column_names[consequent[0]], int_to_operator_opposite[consequent[1]], value)
+ else:
+    df_new = compare(df_new, column_names[consequent[0]], int_to_operator[consequent[1]], value)
+ 
+ return len(df_new.index)
+
+def prune(df, initial_error, Y1, E1, antecedents, consequent, confidence_level):
+ '''
+ Function to prune the rule based on error calcs, will call itself
+ '''
+ index = 0
+ lowest_error = initial_error
+ best_antecedents = antecedents.copy()
+ for antecedent in antecedents:
+    Y2 = find_occurences(df, antecedents, consequent, [index])
+    E2 = find_occurences(df, antecedents, consequent, [index, -1])
+    error = 1-proportion_confint(Y1+Y2, Y1+Y2+E1+E2, confidence_level, method='beta')[0] 
+    if error < lowest_error:
+        new_antecedents = antecedents.copy()
+        lowest_error = error
+        best_antecedents = new_antecedents
+        new_antecedents.remove(antecedent)
+        new_Y1 = find_occurences(df, new_antecedents, consequent, [])
+        new_E1 = find_occurences(df, new_antecedents, consequent, [-1])
+        new_initial_error = 1-proportion_confint(Y1, E1+Y1, confidence_level, method='beta')[0]
+        new_antecedents, _ = prune(df, new_initial_error, new_Y1, new_E1, new_antecedents, consequent, confidence_level)
+    index += 1
+ return [best_antecedents, consequent], lowest_error
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_file', help="Specify the name of the data file", default="credit_output.csv")
+    parser.add_argument('--rules_file', help="Specify the name of the rules file", default="rules.txt")
+    parser.add_argument("--rule", help="Specify a custom rule to be run and pruned", default="")
+    parser.add_argument('--type', help="Specify the type of operation to be run", choices=["PRUNE", "RULES"], default="PRUNE")
+    args = parser.parse_args()
+    # Modify this to run tests at a different alpha
+    confidence_level = 0.75
+
+    if args.type == "RULES":
+        '''
+        This generates rules in the specified format, given a weka format
+        decision tree
+        '''
+        rules = open(args.rules_file, "r")
+        df = pd.read_csv(args.data_file)
+        generate_rules(df, rules)
+        print("Boo")
+    else:
+        '''
+        This runs through all the rules provided, and tries to prune them
+        '''
+        df = pd.read_csv(args.data_file)
+        rules = []
+        if args.rule == "":
+            ruleFile = open(args.rules_file, "r")
+            rules = ruleFile.readlines()
+        else:
+            rules.append(args.rule)
+
+        antecedents = []
+        consequent = []
+
+        for rule in rules:
+            print("-----------Rule:")
+            print(rule)
+            print()
+            print(f"Trying to prune with confidence level {1-confidence_level}")
+            rule = ast.literal_eval(rule)
+
+            antecedents = rule[0]
+            consequent = rule[1]
+            Y1 = find_occurences(df, antecedents, consequent, [])
+            E1 = find_occurences(df, antecedents, consequent, [-1])
+
+            print(Y1)
+            print(E1)
+
+            if(E1 == 0 and Y1 == 0):
+                print("Could not find any occurences of this rule in the data set!")
+                print()
+                continue
+            
+            initial_error = 1-proportion_confint(Y1, E1+Y1, confidence_level, method='beta')[0]
+            better_rule, new_error = prune(df, initial_error, Y1, E1, antecedents, consequent, confidence_level)
+            
+            if(new_error != initial_error):
+                print(f"Pruned rule: {better_rule} improved error from {initial_error} to {new_error}")
+            else:
+                print(f"Found no better version, rule still remains: {better_rule}") 
+                pass
+            print()
